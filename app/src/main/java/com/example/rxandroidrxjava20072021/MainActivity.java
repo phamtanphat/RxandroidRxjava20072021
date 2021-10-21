b2f8c7a33295b4f2bb19eea40affbcc0c5c8c2b7
phamtanphat <phatdroid94@gmail.com>
2021-10-21 19:17:56 +0700

    future

--- app/src/main/java/com/example/rxandroidrxjava20072021/MainActivity.java
+++ app/src/main/java/com/example/rxandroidrxjava20072021/MainActivity.java
@@ -6,40 +6,49 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Build;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.util.Log;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.FutureTask;
 import java.util.function.Consumer;
 
 public class MainActivity extends AppCompatActivity {
 
-    @RequiresApi(api = Build.VERSION_CODES.N)
+    int a = 0;
+    private ExecutorService executors;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ArrayList<String> mangTen = new ArrayList<>();
-        mangTen.add("A");
-        mangTen.add("B");
-        mangTen.add("C");
-        mangTen.add("D");
+        executors = Executors.newFixedThreadPool(1);
 
-        Iterable<String> data = new Iterable<String>() {
-            @NonNull
-            @Override
-            public Iterator<String> iterator() {
-                return mangTen.iterator();
-            }
-        };
+       Future future = executors.submit(new Callable<Object>() {
+           @Override
+           public Object call() throws Exception {
+               Log.d("BBB",Thread.currentThread().getName());
+               Thread.sleep(2000);
+               a = 0;
+               Integer b = null;
+               a = a / b;
+               return a;
+           }
+       });
 
-        data.forEach(new Consumer<String>() {
-            @Override
-            public void accept(String s) {
-                Log.d("BBB",s);
-            }
-        });
+        try {
+            Log.d("BBB",future.get().toString());
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
